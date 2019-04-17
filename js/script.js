34ad74ec10317f93c43f396d5d15d481f8912afc
Jose Horta <joshhortt@yahoo.com>
2019-04-17 19:39:21 +0100

    add business logic '.ready()' function to script.js'.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 
+// Business (or back-end) logic:
 var add = function(number1, number2){
 	return number1 + number2;
 };
@@ -15,16 +16,13 @@ var divide = function(number1, number2){
   return number1 / number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+// Everything below this line is user interface (or front-end) logic:
 
-// here we only use the add variable
-alert(add(number1, number2));
-
-// manually change that line to choose between operations
-
-/*
-alert(subtract(number1, number2));
-alert(multiply(number1, number2));
-alert(divide(number1, number2));
-*/
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    alert(add(number1, number2));
+  });
+});
