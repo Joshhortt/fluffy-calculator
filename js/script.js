214fa77473c9935ad65dd0b9af6bf6205c3b8101
Jose Horta <joshhortt@yahoo.com>
2019-04-24 11:22:51 +0100

    update scripts.js to point to the newly-updated id's when attaching the form submit listener and reading the input values.

--- js/script.js
+++ js/script.js
@@ -19,10 +19,11 @@ var divide = function(number1, number2){
 // Everything below this line is user interface (or front-end) logic:
 
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+  $("form#calculator").submit(function(event) {
     event.preventDefault();
-    var number1 = parseInt($("#add1").val());
-    var number2 = parseInt($("#add2").val());
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+		var operator = $("input:radio[name=operator]:checked").val();
 		var result = add(number1, number2);
     $("#output").text(result);
   });
